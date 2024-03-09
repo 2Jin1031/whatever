7e1857b2ff66d4f5870fce8886ac563aaa9e8fcc
2jin2.1031 <2jin2.1031@gmail.com>
2024-03-09 16:50:14 +0900

    Chore: Remove unused imports

--- src/main/java/zkffl0/whatever/controller/MemberController.java
+++ src/main/java/zkffl0/whatever/controller/MemberController.java
@@ -1,16 +1,15 @@
 package zkffl0.whatever.controller;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.webjars.NotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import zkffl0.whatever.common.exception.MemberDuplicateException;
-import zkffl0.whatever.dto.comment.CommentReqDto;
 import zkffl0.whatever.dto.member.sign_up.SignUpReqDto;
 import zkffl0.whatever.dto.member.sign_up.SignUpResDto;
-import zkffl0.whatever.dto.post.PostReqDto;
-import zkffl0.whatever.service.CommentService;
 import zkffl0.whatever.service.MemberService;
 import zkffl0.whatever.utils.swagger.account.RegisterReqApi;
 import zkffl0.whatever.utils.swagger.account.RegisterResApi;
@@ -38,4 +37,24 @@ public class MemberController {
         }
     }
 
+    // TODO : 로그인 관련 공부 좀 더 하고 로직 추가
+    /** 로그인 */
+//    @LoginReqApi
+//    @LoginResApi
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody LoginReqDto memberInfo) {
+//        try {
+//            LoginResDto loginResDto = authService.login(memberInfo);
+//            return ResponseEntity.status(HttpStatus.OK)
+//                    .body(loginResDto);
+//        }
+//        catch(NotFoundException e){
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                    .body(e.getMessage());
+//        }
+//        catch(InvalidPasswordException e){
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//                    .body(e.getMessage());
+//        }
+//    }
 }
